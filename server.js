33d9c9ac9f999d8fe54fd6ca4c4c0f04cf6d83fc
Cristopher Martarello <crisrossi1313@gmail.com>
2024-11-24 18:54:03 -0300

    feat: adição da lógica de criptografia e descriptografia com node rsa

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const http = require('http');
 const socketIo = require('socket.io');
 const dgram = require('dgram');
 const cors = require('cors');
-const net = require('net');
+const NodeRSA = require('node-rsa');
+const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
 const { clearInterval } = require('timers');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,7 +18,7 @@ const io = socketIo(server, {
     }
 });
 
-const multicastAddress = '224.0.0.5'; // Endereço multicast atualizado
+const multicastAddress = '224.0.0.5'; // Endereço multicast
 const multicastPort = 41234; // Porta multicast
 
 const udpServer = dgram.createSocket('udp4');
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-let clients = [], timer = null;
+let clients = [], timer = null, symmetricKey;
 
 // Dados do leilão
 let currentItem = {
@@ -46,6 +46,8 @@ io.on('connection', (socket) => {
 
     if (clients.length === 1) {
         startTimer();
+    } else if (!clients.length > 0) {
+        stopTimer();
     }
 
     socket.emit('currentItem', currentItem); // Publicar no multicast o item disponível
@@ -121,11 +123,29 @@ const getPublicKey = (cpf) => {
     return null;
 };
 
+const getName = (cpf) => {
+    const filePath = path.join(__dirname, 'certs', cpf, `${cpf}.json`);
+
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        
+        const parsedData = JSON.parse(fileContent.trim());
+
+        return parsedData.name || null;
+    }
+
+    return null;
+}
+
 const encryptWithPublicKey = (publicKey, message) => {
-    const buffer = Buffer.from(message, 'utf8');
-    const encrypted = crypto.publicEncrypt(publicKey, buffer);
-    return encrypted.toString('base64');
+    const key = new NodeRSA(publicKey);
+    key.setOptions({ encryptionScheme: 'pkcs1' });
+    return key.encrypt(message, 'base64');
 };
+
+const normalizeKey = (key) => key.replace(/(\r\n|\n|\r)/gm, '').trim();
+
+const generateSymmetricKey = () => crypto.randomBytes(32).toString('hex');
 
 udpServer.on('message', (msg, rinfo) => {
     const serverAddress = udpServer.address().address;
@@ -151,30 +171,38 @@ udpServer.bind(multicastPort, () => {
 });
 
 app.post('/authenticate', (req, res) => {
-    console.log(req.body);
-    const { cpf } = req.body;
+    const { cpf, publicKey } = req.body;
 
     if (isValidCPF(cpf)) {
-        const publicKey = getPublicKey(cpf);
+        const storedPublicKey = getPublicKey(cpf);
+        const storedName = getName(cpf);
 
-        if (publicKey) {
-            const encryptedMessage = encryptWithPublicKey(publicKey, 'Mensagem segura');
+        if (!storedPublicKey) {
+            return res.status(400).json({ success: false, message: 'Chave pública não encontrada para validação.' });
+        }
+
+        if (normalizeKey(storedPublicKey) === normalizeKey(publicKey)) {
+            const user = { nome: storedName, cpf }
+
+            const encryptedSymmetricKey = encryptWithPublicKey(publicKey, symmetricKey);
+            const encryptedUserInfo = encryptWithPublicKey(publicKey, user);
+            const encryptedMulticastAddress =  encryptWithPublicKey(publicKey, multicastAddress);
 
             res.json({
                 success: true,
-                user: { nome: 'teste', cpf: '12345678900' },
-                encryptedMessage: encryptedMessage
+                encryptedUserInfo,
+                encryptedSymmetricKey,
+                encryptedMulticastAddress,
             });
-
         } else {
-            res.json({ success: false, message: 'Chave pública não encontrada para este CPF.' });
+            res.status(400).json({ success: false, message: 'Chave pública inválida ou não encontrada.' });
         }
-
     } else {
-        res.json({ success: false, message: 'CPF inválido' });
+        res.status(400).json({ success: false, message: 'CPF inválido.' });
     }
 });
 
 server.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
+    symmetricKey = generateSymmetricKey(); // Chave simétrica ao iniciar server
 });
